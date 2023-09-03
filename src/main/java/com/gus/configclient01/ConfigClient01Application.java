a13bcbb4c4bff85f04b168d515b0a3fcbd91ba07
Gustavo Avila <gustavogavila01@gmail.com>
2023-09-03 11:27:48 -0300

    Inicio do client

---
+++ src/main/java/com/gus/configclient01/ConfigClient01Application.java
@@ -0,0 +1,13 @@
+package com.gus.configclient01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClient01Application.class, args);
+	}
+
+}
